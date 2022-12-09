987dbae4988a620fa222bd741a59af24885922ac
Mohamad <no.future2e1@gmail.com>
2022-12-09 10:31:35 +0200

    wip
    Signed-off-by: Mohamad <no.future2e1@gmail.com>

--- packages/dashboard/src/components/Box.tsx
+++ packages/dashboard/src/components/Box.tsx
@@ -2,23 +2,33 @@ import * as THREE from 'three';
 import { createRoot } from 'react-dom/client';
 import React, { useRef, useState } from 'react';
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
+import { Euler, StringKeyframeTrack } from 'three';
 
-export function Box(props: ThreeElements['mesh']) {
+export interface BoxProps {
+  position: number[];
+  // thickness, length, height
+  rot: THREE.Euler;
+  size: number[];
+  meshRef?: React.Ref<THREE.Mesh>;
+  color?: string;
+}
+
+export function Box(
+  { position, size, rot, color, meshRef }: BoxProps,
+  props: ThreeElements['mesh'],
+) {
   const ref = useRef<THREE.Mesh>(null!);
-  const [hovered, hover] = useState(false);
-  const [clicked, click] = useState(false);
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+  // ref.current.rotateY(0.1);
+
   return (
     <mesh
       {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
+      position={[position[0], position[1], position[2]]}
+      rotation={rot}
+      ref={meshRef || ref}
     >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <boxGeometry args={[size[0], size[1], size[2]]} />
+      <meshStandardMaterial color={color || 'blue'} />
     </mesh>
   );
 }
