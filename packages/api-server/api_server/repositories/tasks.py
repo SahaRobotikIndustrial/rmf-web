7e735f2bc95be69fd6d88b5a44daa20fdba716f2
Aaron Chong <aaronchongth@gmail.com>
2022-12-10 01:12:01 +0800

    Commenting out lines to represent data models before migration
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/api-server/api_server/repositories/tasks.py
+++ packages/api-server/api_server/repositories/tasks.py
@@ -33,14 +33,14 @@ class TaskRepository:
                 "category": task_state.category.__root__
                 if task_state.category
                 else None,
-                "assigned_to": task_state.assigned_to.name
-                if task_state.assigned_to
-                else None,
+                # "assigned_to": task_state.assigned_to.name
+                # if task_state.assigned_to
+                # else None,
                 "unix_millis_start_time": task_state.unix_millis_start_time
                 and datetime.fromtimestamp(task_state.unix_millis_start_time / 1000),
                 "unix_millis_finish_time": task_state.unix_millis_finish_time
                 and datetime.fromtimestamp(task_state.unix_millis_finish_time / 1000),
-                "status": task_state.status if task_state.status else None,
+                # "status": task_state.status if task_state.status else None,
             },
             id_=task_state.booking.id,
         )
@@ -53,6 +53,12 @@ class TaskRepository:
                 query = add_pagination(query, pagination)
             # TODO: enforce with authz
             results = await query.values_list("data", flat=True)
+            # for r in results:
+            #     fresh_state = TaskState(**r)
+            #     await self.save_task_state(fresh_state)
+            #     print('saved a task state')
+            # print('doneeee')
+
             return [TaskState(**r) for r in results]
         except FieldError as e:
             raise HTTPException(422, str(e)) from e
